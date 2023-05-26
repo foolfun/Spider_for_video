82243b7b90ad189c428671cc7d713724b93ad939
Audrey <foolfun@163.com>
2023-05-26 19:30:50 +0800

    Add files via upload

--- scrapy_b.py
+++ scrapy_b.py
@@ -14,14 +14,15 @@ import datetime
 # 这些始终要用到，作为全局，可以加快code运行速度
 chrome_options = webdriver.ChromeOptions()
 # 使用headless无界面浏览器模式
-chrome_options.add_argument('--headless') # 增加无界面选项
-chrome_options.add_argument('--disable-gpu') #如果不加这个选项，有时定位会出现问题
+chrome_options.add_argument('--headless')  # 增加无界面选项
+chrome_options.add_argument('--disable-gpu')  # 如果不加这个选项，有时定位会出现问题
 # 启动浏览器
 driver = webdriver.Chrome(options=chrome_options)
 # 隐式等待
 driver.implicitly_wait(10)
 # 防止被识别， 设置随机等待秒数
 rand_seconds = random.choice([1, 3]) + random.random()
+
 
 # 给url获取soup
 def get_soup(url):
@@ -39,7 +40,7 @@ def getFirstContent(soup):
     # soup = BeautifulSoup(content, "html.parser")
     # 搜索的页面出来得到视频部分信息
     next_urls = []
-    infos = soup.find_all('a','bangumi-title')
+    infos = soup.find_all('a', 'bangumi-title')
     for info in infos:
         next_urls.append(info['href'].strip())
     # print(len(infos))
@@ -48,7 +49,7 @@ def getFirstContent(soup):
 
 
 # 获取番剧的一些信息
-def getDetail(path,fname_detail):
+def getDetail(path, fname_detail):
     links_ = pd.read_csv(path)
     links = links_.drop_duplicates()  # 可能有重复的需要去重
     urls = links['links']
@@ -75,12 +76,10 @@ def getDetail(path,fname_detail):
                  <li class="">相关视频</li>]
             '''
             # 评分数， '长评 ( 572 )' 取数字572,变为int,没有评论等信息的不需要，进行跳过
-            try:
-                long = int(soup2.find('div', 'media-tab-nav').find('ul').find_all('li')[1].string[5:-2])
-                short = int(soup2.find('div', 'media-tab-nav').find('ul').find_all('li')[2].string[5:-2])
-            except:
-                long=0
-                short=0
+
+            long = int(soup2.find('div', 'media-tab-nav').find('ul').find_all('li')[1].string[5:-2])
+            short = int(soup2.find('div', 'media-tab-nav').find('ul').find_all('li')[2].string[5:-2])
+
             long_comms.append(long)
             short_comms.append(short)
             # 取标题
@@ -92,11 +91,11 @@ def getDetail(path,fname_detail):
                 tags = tags + str(tag.string) + ','  # tags='漫画改,战斗,热血,声控,'
             genres.append(tags)
             # 截取年份：'2019年4月7日开播'
-            year = soup2.find('div','media-info-time').span.string
+            year = soup2.find('div', 'media-info-time').span.string
             years.append(year)
 
             # 增加id的
-            v_ids.append(soup1.find('a','mediainfo_avLink__bN7nf').string)
+            v_ids.append(soup1.find('a', 'mediainfo_avLink__bN7nf').string)
             cont_id += 1
             # v_ids.append(cont_id)
             # 获取当前页面链接
@@ -128,7 +127,7 @@ def getDetail(path,fname_detail):
 
 
 # 获取番剧的相关推荐
-def getRecommond(path,fname_detail):
+def getRecommond(path, fname_detail):
     detail_data = pd.read_csv(path)
     detail_data_ = detail_data.drop_duplicates()  # 可能有重复的需要去重
     urls = detail_data_['detail_link']
@@ -143,20 +142,20 @@ def getRecommond(path,fname_detail):
             # 增加count
             cont_id += 1
 
-            v_ids.append(detail_data_.loc[cont_id,'v_id'])
+            v_ids.append(detail_data_.loc[cont_id, 'v_id'])
             # 获取推荐番剧的title
             tmp_title = []
-            for title in soup1.find_all('div','slide-item-title'):
+            for title in soup1.find_all('div', 'slide-item-title'):
                 tmp_title.append(title.string)
             rec_title.append(tmp_title)
             # 获取推荐番剧的link
             rec_links = []
-            for l in soup1.find_all('div','slide-item-info'):
+            for l in soup1.find_all('div', 'slide-item-info'):
                 rec_links.append(l.find('a')['href'])
             # 获取推荐番剧的id
             tmp_id = []
             for link in rec_links:
-                soup2 = get_soup(r'http:'+link)
+                soup2 = get_soup(r'http:' + link)
                 tmp_id.append(soup2.find('a', 'av-link').string)
 
             rec_id.append(tmp_id)
@@ -167,7 +166,7 @@ def getRecommond(path,fname_detail):
             # 每5条写入一次，防止中断导致数据丢失
             if cont_id % 5 == 0:
                 # 写入
-                Data_detail = {'v_id': v_ids, 'rec_id': rec_id,'rec_title':rec_title}
+                Data_detail = {'v_id': v_ids, 'rec_id': rec_id, 'rec_title': rec_title}
                 wirte2csv(Data_detail, fname_detail)
                 # 清空
                 v_ids = []  # id
@@ -179,7 +178,6 @@ def getRecommond(path,fname_detail):
         except Exception:
             pass
     return
-
 
 
 def process_time(rat_time):
@@ -194,14 +192,14 @@ def process_time(rat_time):
             return (datetime.datetime.now()).strftime("%Y-%m-%d")
 
         elif rat_time == '昨天':
-            return (datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+            return (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
         elif len(rat_time) == 5:  # 如果没有年份
             return str(datetime.datetime.now().year) + '-' + rat_time
 
 
 # 滚动获取评论信息的方法
-def get_rating(url,page_num):
+def get_rating(url, page_num):
     # 获取网页源代码
     driver.get(url)
     # driver.get(url + r'#long')
@@ -216,14 +214,14 @@ def get_rating(url,page_num):
         driver.execute_script(js)
         time.sleep(rand_seconds)
         # 具体看网页是怎么样的，b站是滑动到哪里，上面的都会加载进来，因此选取滑动之后的网页
-        if i == page_num-1:
+        if i == page_num - 1:
             # 获取页面
             content = driver.page_source
             # 放入解析
             soup = BeautifulSoup(content, 'lxml')
             # 找到这页id
-            for li in soup.find_all('li','clearfix'):
-                id_names.append(li.find('div',re.compile('review-author-name')).string.strip())
+            for li in soup.find_all('li', 'clearfix'):
+                id_names.append(li.find('div', re.compile('review-author-name')).string.strip())
                 rat = len(li.find_all('i', 'icon-star icon-star-light'))  # 评分
                 ratings.append(rat)
 
@@ -232,19 +230,20 @@ def get_rating(url,page_num):
                 rat_time_2 = process_time(rat_time)
                 rating_times.append(str(rat_time_2))
 
-    return id_names,ratings,rating_times
+    return id_names, ratings, rating_times
 
 
 # 获取rating，相关信息，并存入csv
 def get_rating_data(path):
     detail = pd.read_csv(path)
-    # print(min(detail['short_comm']+detail['long_comm']))  # 230;
-    # print(detail.columns)  # ['v_id', 'title', 'genres', 'year', 'long_comm', 'short_comm','detail_link']
-    minn = min(detail['short_comm'] + detail['long_comm'])
-    rating_links = detail['detail_link']
     long_num = detail['long_comm']
     short_num = detail['short_comm']
+    rating_links = detail['detail_link']
     v_ids = detail['v_id']
+    # print(min(detail['short_comm']+detail['long_comm']))  # 230;
+    # print(detail.columns)  # ['v_id', 'title', 'genres', 'year', 'long_comm', 'short_comm','detail_link']
+    minn = min(long_num + short_num)  # 获取所有番剧中最少的评论总数，即最小总评论数
+
     for ind, url in enumerate(tqdm(rating_links)):
         # print(ind,url)
         # if ind< 425:
@@ -252,26 +251,27 @@ def get_rating_data(path):
         # 按比例取长短评价
         # print(v_ids[61])
         # 因为长评论会比短评论少，可能用单纯的通过最小总评论数进行比例计算，可能会出现长评数不够，最终会有一些数据的丢失，为此这里进行最小的比较
-        lon = min(int((long_num[ind] / (long_num[ind] + short_num[ind])) * minn),long_num[ind])
+        lon = min(int((long_num[ind] / (long_num[ind] + short_num[ind])) * minn), long_num[ind])
         sho = minn - lon
 
         long_page_num = math.ceil(lon / 20)  # 一页20个数据，看需要滑动几页
         short_page_num = math.ceil(sho / 20)  # 一页20个数据，看需要滑动几页
 
-        id_l, rat_l,time_l = get_rating(url + r'#long', long_page_num)
-        id_s, rat_s,time_s = get_rating(url + r"#short", short_page_num)
+        id_l, rat_l, time_l = get_rating(url + r'#long', long_page_num)
+        id_s, rat_s, time_s = get_rating(url + r"#short", short_page_num)
         # print(len(id_l))
         # print(len(id_s))
 
         # 需要把之前的长短评价各自分配的数目取到
-        id_total = id_l[0:lon]+id_s[0:sho]
-        rat_total = rat_l[0:lon]+rat_s[0:sho]
-        rating_time_total = time_l[0:lon]+time_s[0:sho]
+        id_total = id_l[0:lon] + id_s[0:sho]
+        rat_total = rat_l[0:lon] + rat_s[0:sho]
+        rating_time_total = time_l[0:lon] + time_s[0:sho]
         # print(len(id_total))
         # print(len(rat_total))
 
         # 封装到DataFrame
-        Data_rating = {'user_id_name': id_total,'v_id':[v_ids[ind]]*minn,'rating':rat_total,'rating_time':rating_time_total}
+        Data_rating = {'user_id_name': id_total, 'v_id': [v_ids[ind]] * minn, 'rating': rat_total,
+                       'rating_time': rating_time_total}
         # print(Data_rating)
         fname_rating = "rating_data.csv"
         wirte2csv(Data_rating, fname_rating)
@@ -279,7 +279,7 @@ def get_rating_data(path):
 
 
 # 写入csv
-def wirte2csv(Data,fname):
+def wirte2csv(Data, fname):
     try:
         if os.path.exists(fname):
             DataFrame = pd.DataFrame(Data)
@@ -287,15 +287,15 @@ def wirte2csv(Data,fname):
             print('追加成功！')
         else:
             DataFrame = pd.DataFrame(Data)
-            DataFrame.to_csv(fname, index=False, sep=',')
+            DataFrame.to_csv(fname, index=False, sep=',', header=True)
             print('save!')
     except:
         print('fail')
 
 
 if __name__ == '__main__':
-    flag1 = 1  # 要不要爬取番剧列表页
-    flag2 = 1  # 要不要爬取番剧信息
+    flag1 = 0  # 要不要爬取番剧列表页
+    flag2 = 0  # 要不要爬取番剧信息
     flag3 = 1  # 要不要爬取评分
     flag4 = 1  # 要不要爬取相关推荐
     if flag1:
@@ -304,15 +304,15 @@ if __name__ == '__main__':
             # 从0开始的原因是，对于第一次访问的页面会连续访问两次，导致重复爬取，所以i=0时获取页面，但是不去存入信息
             # 剧番页面，从1-20页
             url = 'https://www.bilibili.com/anime/index/#season_version=-1&area=-1' \
-              '&is_finish=-1&copyright=-1&season_status=-1&season_month=-1&year=-1' \
-              '&style_id=-1&order=3&st=1&sort=0&page='+str(i+1)
+                  '&is_finish=-1&copyright=-1&season_status=-1&season_month=-1&year=-1' \
+                  '&style_id=-1&order=3&st=1&sort=0&page=' + str(i + 1)
             #  刷新，重要！！！否则可能会导致重复爬取第一个页面
             driver.refresh()
             # print(url)
             soup = get_soup(url)
             if i == 0:
                 continue
-            #driver.find_element_by_class_name('p next-page').click()
+            # driver.find_element_by_class_name('p next-page').click()
             next_urls = getFirstContent(soup)
             print(next_urls)
             # 写入csv
@@ -326,7 +326,7 @@ if __name__ == '__main__':
         # step2
         path = r'./link_data.csv'
         # 爬取细节并存入新的csv
-        getDetail(path,fname_detail = "video_data.csv")
+        getDetail(path, fname_detail="video_data.csv")
     if flag3:
         # step3
         detail_data_path = r'./video_data.csv'
